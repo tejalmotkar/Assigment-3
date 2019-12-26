5ebbc975fe90002b79c9faaa0caf116430362ce0
Tejal Motkar <Tejal.Motkar@cytel.pune.in.local>
2019-12-26 13:24:36 +0530

    uploading project files

---
+++ js/index.js
@@ -0,0 +1,137 @@
+$(document).ready(function() {
+  $('#signup-btn').click(function(event) {
+      resetSignupForm();
+      $("#signup-modal").modal();
+  });
+
+  $(".singup").click(function(event) {
+    var firstName = $("#firstName");
+    var lastName = $("#lastName");
+    var email = $("#signupemail");
+    var password = $("#signup-password");
+    var confirmPassword = $("#signup-confirm-password");
+    var errorMessageId = "#signup-error-message";
+    if(firstName.val().trim() === "") {
+        setErrorMessage("Please enter first name.", errorMessageId, firstName);
+        event.preventDefault();
+        return false;
+    } else if(lastName.val().trim() === "") {
+        setErrorMessage("Please enter last name.", errorMessageId, lastName);
+        event.preventDefault();
+        return false;
+    } else if(email.val().trim() === "") {
+        setErrorMessage("Please enter email address.", errorMessageId, email);
+        event.preventDefault();
+        return false;
+    } else if(!validateEmail(email, errorMessageId)) {
+      return false;
+    } else if(password.val() === "") {
+        setErrorMessage("Please enter password.", errorMessageId, password);
+        event.preventDefault();
+        return false;
+    } else if(password.val().trim() === "") {
+        setErrorMessage("Space not allowed in password.", errorMessageId, password);
+        event.preventDefault();
+        return false;
+    } else if(confirmPassword.val() === "") {
+        setErrorMessage("Please enter confirm password.", errorMessageId, confirmPassword);
+        event.preventDefault();
+        return false;
+    } else if(confirmPassword.val().trim() === "") {
+        setErrorMessage("Space not allowed in confirm password.", errorMessageId, confirmPassword);
+        event.preventDefault();
+        return false;
+    } else if(password.val() !== confirmPassword.val()) {
+        setErrorMessage("Password and confirm password should match.", errorMessageId, password);
+        event.preventDefault();
+        return false;
+    }
+    $("#success").removeClass("hide");
+    $("#modal-container").addClass("hide");
+    sessionStorage.setItem("firstName", firstName.val().trim());
+    sessionStorage.setItem("lastName", lastName.val().trim());
+    sessionStorage.setItem("userName", email.val().trim());
+    sessionStorage.setItem("password", password.val().trim());
+    event.preventDefault();
+  });
+
+  $(".singup-close").click(function() {
+      resetSignupForm();
+  });
+
+  $('#login-btn').click(function(event) {
+      resetLoginForm();
+      $("#login-modal").modal();
+  });
+
+  $(".login-form").click(function(event) {
+    var password = $("#login-password");
+    var email = $("#login-email");
+    var errorMessageId = "#login-error-message";
+
+    if(email.val() === "") {
+      setErrorMessage("Please enter username.", errorMessageId, email);
+      return false;
+    }
+
+    if(!validateEmail(email, errorMessageId)) {
+      return false;
+    }
+
+    if(password.val() === "") {
+      setErrorMessage("Please enter password.", errorMessageId, password);
+      return false;
+    }
+
+     if(!(email.val().toLowerCase() === sessionStorage.getItem("userName") && password.val() ===  sessionStorage.getItem("password"))) {
+       setErrorMessage("Invalid username and password.", errorMessageId, "");
+       return false;
+    }
+
+    //$('#navbar').load('./navbar.html');
+
+  });
+
+  $(".login-close").click(function() {
+    resetLoginForm();
+  });
+
+  function resetLoginForm() {
+    $("#login-error-message").text("");
+    $("#login-error-message").removeClass("hide");
+    $("#login-password").val("");
+    $("#login-email").val("");
+  }
+
+  function resetSignupForm() {
+    $("#signup-error-message").text("");
+    $("#signup-error-message").removeClass("hide");
+    $("#modal-container").removeClass("hide");
+    $("#success").addClass("hide");
+    $("#firstName").val("");
+    $("#lastName").val("");
+    $("#signupemail").val("");
+    $("#signup-password").val("");
+    $("#signup-confirm-password").val("");
+  }
+
+  function setErrorMessage(message, errorMessageselector, focusSelector) {
+    var errorMessage = $(errorMessageselector);
+    errorMessage.removeClass("hide");
+    errorMessage.show();
+    errorMessage.text(message);
+    if(focusSelector !== undefined && focusSelector.length > 0) {
+      focusSelector.focus();
+    }
+  }
+
+  function validateEmail(inputText, errorMessageselector) {
+    var mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+    if(!($(inputText).val().match(mailformat))) {
+      setErrorMessage("You have entered an invalid email address.", errorMessageselector, inputText);
+      return false;
+    }
+
+    return true;
+  }
+});
---
+++ js/request.js
@@ -0,0 +1,98 @@
+$(document).ready(function() {
+  $('.new-request-btn').click(function(event) {
+      resetRequestForm()
+      $("#new-request-modal").modal();
+  });
+
+  $('.add-request-btn').click(function(event) {
+    var subject = $("#subject");
+    var priority = $("#priority");
+    var name = $("#name");
+    var contactNumber = $("#contactNumber");
+    var description = $("#description");
+    var errorMessageId = "#request-error-message";
+    var phonenoPattern = /^\d{10}$/;
+
+    if(subject.val().trim() === "") {
+        setErrorMessage("Please enter subject name.", errorMessageId, subject);
+        event.preventDefault();
+        return false;
+    } else if(priority.val().trim() === "") {
+        setErrorMessage("Please select priority.", errorMessageId, priority);
+        event.preventDefault();
+        return false;
+    } else if(name.val().trim() === "") {
+        setErrorMessage("Please enter name.", errorMessageId, name);
+        event.preventDefault();
+        return false;
+    } else if(contactNumber.val().trim() === "") {
+        setErrorMessage("Please enter contact number.", errorMessageId, contactNumber);
+        event.preventDefault();
+        return false;
+    }  else if(contactNumber.val().length < 10) {
+        setErrorMessage("Contact number should be 10 digit.", errorMessageId, contactNumber);
+        event.preventDefault();
+        return false;
+    } else if(!contactNumber.val().match(phonenoPattern)) {
+        setErrorMessage("Contact number should be digit.", errorMessageId, contactNumber);
+        event.preventDefault();
+        return false;
+    } else if(description.val().trim() === "") {
+        setErrorMessage("Please enter description.", errorMessageId, description);
+        event.preventDefault();
+        return false;
+    }
+
+    $(".request-table").removeClass("hide");
+    var date = new Date();
+    var currentdate = date.getDate() +"/"+ date.getMonth() +"/"+ date.getFullYear();
+    var duedate = date.getDate()  +"/"+ date.getMonth() +"/"+ date.getFullYear();
+    var markup = "<tr><td> " + subject.val() + "</td><td>"+ currentdate  + "</td><td>" + duedate + "</td><td>"+ name.val()+"</td><td>Active</td></tr>";
+    $(".request-table tbody").append(markup);
+    $("#new-request-modal").modal('hide')
+  });
+
+  $(".request-close").click(function() {
+    resetRequestForm();
+  });
+
+  function setErrorMessage(message, errorMessageselector, focusSelector) {
+    var errorMessage = $(errorMessageselector);
+    errorMessage.removeClass("hide");
+    errorMessage.show();
+    errorMessage.text(message);
+    if(focusSelector !== undefined && focusSelector.length > 0) {
+      focusSelector.focus();
+    }
+  }
+
+  function resetRequestForm() {
+    $("#request-error-message").text("");
+    $("#request-error-message").removeClass("hide");
+    $("#subject").val("");
+    $("#priority").val("");
+    $("#name").val("");
+    $("#contactNumber").val("");
+    $("#description").val("");
+  }
+
+  $("#detail-tab").click(function () {
+    $("#detailtab").removeClass("hide");
+    $("#home").addClass("hide");
+    $("#home-tab-li").removeClass("active");
+    $("#detail-tab-li").addClass("active");
+    if($("#detailsTable tr").length == 1) {
+      markup = "<tr><td> " + sessionStorage.getItem("firstName") + "</td><td>"+ sessionStorage.getItem("lastName")  + "</td><td>" + sessionStorage.getItem("userName") + "</td></tr>";
+      tableBody = $("#detailsTable tbody");
+      tableBody.append(markup);
+    }  
+  });
+
+  $("#home-tab").click(function () {
+      $("#detailtab").addClass("hide");
+      $("#home").removeClass("hide");
+      $("#home-tab-li").addClass("active");
+      $("#detail-tab-li").removeClass("active");
+  });
+
+});
